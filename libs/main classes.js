3ecd3b088e2210d21f328fe6b2312d58c6745b1b
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-19 20:32:08 +0300

    Update main classes.js

--- libs/main classes.js
+++ libs/main classes.js
@@ -1,11 +1,12 @@
 class Div{
-  constructor({path, addBefore, cName, id, text, label, title, name, tab, value, valueName, editable, style, onclick, onkeydown, onkeyup, onwheel, onfocus, onblur, onmouseenter, onmouseleave, rtn, func}){
+  constructor({path, addBefore, cName, id, text, label, title, name, group, tab, value, valueName, editable, style, onclick, onkeydown, onkeyup, onwheel, onfocus, onblur, onpaste, onmouseenter, onmouseleave, rtn, func}){
     this.main=document.createElement('div');
     if(cName) this.main.className=cName;
     if(id) this.main.id=id;
     if(text) this.main.textContent=text;
     if(title) this.main.title=title;
     if(name) this.main.setAttribute('name', name);
+    if(group) this.main.setAttribute('group', group);
     if(tab) this.main.tabIndex=tab;
     if(editable) this.main.setAttribute('contenteditable', true);
     if(style) this.main.style=style;
@@ -15,6 +16,7 @@ class Div{
     if(onwheel) this.main.onwheel=onwheel;
     if(onfocus) this.main.onfocus=onfocus;
     if(onblur) this.main.onblur=onblur;
+    if(onpaste) this.main.onpaste=onpaste;
     if(onmouseenter) this.main.onmouseenter=onmouseenter;
     if(onpointerenter) this.main.onpointerenter=onpointerenter;
     if(label) this.label=new Div({
@@ -37,13 +39,14 @@ class Div{
 };
 
 class Button{
-  constructor({path, cName, id, text, label, style, onclick, rtn}){
+  constructor({path, cName, id, text, label, style, onclick, disabled, rtn}){
     this.main=document.createElement('button');
     if(cName) this.main.className=cName;
     if(id) this.main.id=id;
     if(text) this.main.textContent=text;
     if(style) this.main.style=style;
     if(onclick) this.main.onclick=onclick;
+    if(disabled) this.main.disabled=disabled;
     path.appendChild(this.main);
 
     if(label) this.mainLabel=new Div({
@@ -89,7 +92,7 @@ class Video{
     if(muted) this.main.muted=muted;
     if(loop) this.main.loop=loop;
     if(controls) this.main.controls=controls;
-    if(pIp) this.main.disablePictureInPicture=pIp;
+    pIp ? this.main.disablePictureInPicture=false : this.main.disablePictureInPicture=true;
     if(style) this.main.style=style;
     if(onclick) this.main.onclick=onclick;
     if(onplay) this.main.onplay=onplay;
@@ -109,7 +112,7 @@ class Video{
 };
 
 class Select{
-  constructor({path, container, label, name, value, body, options, onchange, rtn}){
+  constructor({path, container, label, name, value, body, options, optgroups, onchange, rtn}){
     class Option{
       constructor({path, text}){
         this.main=document.createElement('option');
@@ -148,7 +151,8 @@ class Select{
     if(container){
       this.container=new Div({
         path: path,
-        cName: 'container',
+        cName: typeof container === 'boolean' ? 'container' : container.cName,
+        group: container.g,
         rtn: []
       });
     }
@@ -164,6 +168,14 @@ class Select{
       new Option({
         path: this.main,
         text: e
+      })
+    });
+    if(optgroups) optgroups.forEach(e => {
+      new Optgroup({
+        path: this.main,
+        label: e.label,
+        option: e.option,
+        options: e.options
       })
     });
     this.main.value=value;
@@ -186,11 +198,12 @@ class Select{
 };
 
 class Input{
-  constructor({path, container, type, cName, name, id, text, label, title, value, autocomplete, list, accepted, pattern, placeholder, min, max, step, checked, disabled, required, auto, onchange, onfocus, onblur, onkeydown, onkeyup, style, rtn}){
+  constructor({path, container, type, cName, name, id, text, label, title, value, autocomplete, list, accepted, pattern, placeholder, min, max, step, checked, disabled, required, auto, onclick, onchange, onfocus, onblur, onpaste, onkeydown, onkeyup, style, rtn}){
     if(container){
       this.container=new Div({
         path: path,
-        cName: 'container',
+        cName: container.n ? `container ${container.n}` : 'container',
+        group: container.g,
         rtn: []
       });
     };
@@ -220,9 +233,11 @@ class Input{
     if(max) this.main.max=max;
     if(step) this.main.step=step;
     if(auto) this.main.autocomplete=auto;
+    if(onclick) this.main.onclick=onclick;
     if(onchange) this.main.onchange=onchange;
     if(onfocus) this.main.onfocus=onfocus;
     if(onblur) this.main.onblur=onblur;
+    if(onpaste) this.main.onpaste=onpaste;
     if(onkeydown) this.main.onkeydown=onkeydown;
     if(onkeyup) this.main.onkeyup=onkeyup;
     // if(!container) path.appendChild(this.main);
@@ -233,10 +248,10 @@ class Input{
     if(label){
       this.label=document.createElement('label');
       this.label.className='input-label';
-      if(label[1]) this.label.htmlFor=this.main.id;
-      this.label.textContent=label[0];
-      path.appendChild(this.label);
-      // (container ? this.container : this.label).appendChild(this.main);
+      // if(label[1]) this.label.htmlFor=this.main.id;
+      this.label.textContent=label;
+      // path.appendChild(this.label);
+      (container ? this.container : path).appendChild(this.label);
     };
 
     if(rtn){
@@ -247,12 +262,162 @@ class Input{
         this.obj[e] = this[e];
       })
       return this.obj;
+    };
+  }
+};
+
+class List{
+  Li({t}){
+    let main=document.createElement('li');
+    main.textContent=t;
+    this.main.appendChild(main);
+  }
+  constructor({path, cName, id, list, lists}){
+    this.main=document.createElement('ul');
+    if(cName) this.main.className=cName;
+    if(id) this.id=id;
+    if(list) list.forEach(e => {
+      this.Li({
+        path: this.main,
+        t: e
+      });
+    });
+    if(lists) lists.forEach(e => {
+      this.Li({
+        path: this.main,
+        t: e.t
+      });
+    });
+    path.appendChild(this.main);
+  }
+}
+
+class Field{
+  auto(cnt, cnts, name, cfg){
+    // console.log(cfg)
+    const getter = (t, path) => path.split('.').reduce((r, k) => r?.[k], t);
+    if(!cnt && !cnts) return cfg[name];
+    else
+    if(cnt && cnt.g) return getter(cfg, `${cnt.g}.${name}`);
+    else
+    if(cnts && cnts.g) return getter(cfg, `${cnts.g}.${name}`);
+    else return cfg[name];
+  }
+  constructor({path, groupName, cName, dontRead, legend, info, inputs, inputsAuto, select, style, form, func, rtn}){
+    this.main=document.createElement('fieldset');
+    this.main.groupName=this.main.setAttribute('groupName', groupName);
+    dontRead ? this.main.setAttribute('dontread', true) : '';
+    cName ? this.main.className=cName : '';
+    if(style) this.main.style=style;
+    path.appendChild(this.main);
+
+    if(legend){
+      this.legend=document.createElement('legend');
+      this.legend.textContent=legend;
+      this.legend.onclick=() => {
+        this.main.classList.toggle('hide');
+      }
+      this.main.appendChild(this.legend);
+    }
+    if(info) new Div({
+      path: this.main,
+      cName: 'info',
+      text: info
+    });
+    this.list=new Div({
+      path: this.main,
+      cName: 'list',
+      rtn: []
+    });
+
+    if(inputs) inputs.list.forEach(e => {
+      let type;
+      if(e.type === 'checkbox') type = 'checked';
+      else type = 'value';
+      new Input({
+        path: this.list,
+        type: e.type,
+        name: e.name,
+        title: e.title,
+        [type]: inputs.a ? this.auto(e.c, inputs.c, e.name, inputs.a.cfg[groupName]) : e[type],
+        accepted: e.accepted,
+        number: e.number,
+        min: e.min,
+        max: e.max,
+        step: e.step,
+        // checked: e.checked,
+        text: e.text,
+        label: e.label,
+        iText: e.iText,
+        onchange: e.onchange,
+        num: e.num,
+        container: e.c ? e.c : inputs.c
+      })
+    });
+    // get value(){
+    //   return getter(mainCfg[test.groupName], this.c.g, this.name)
+    // }
+    if(inputsAuto) inputsAuto.list.forEach(e => {
+      new Input({
+        path: this.list,
+        type: e.type,
+        name: e.name,
+        title: e.title,
+        value: getter(inputsAuto.cfg[groupName], e.c ? e.c : inputsAuto.c.g, e.name),
+        accepted: e.accepted,
+        number: e.number,
+        min: e.min,
+        max: e.max,
+        step: e.step,
+        checked: e.checked,
+        text: e.text,
+        label: e.label,
+        iText: e.iText,
+        onchange: e.onchange,
+        num: e.num,
+        container: e.c ? e.c : inputsAuto.c
+      })
+    });
+
+    if(select) select.list.forEach(e => {
+      new Select({
+        path: this.list,
+        label: e.label,
+        name: e.name,
+        value: select.a ? this.auto(e.c, select.c, e.name, select.a.cfg[groupName]) : e.value,
+        options: e.options,
+        optgroups: e.optgroups,
+        container: e.c ? e.c : select.c
+      })
+    });
+
+    if(form) new Form({
+      path: this.list,
+      cName: form.cName,
+      id: form.id,
+      name: form.name,
+      action: form.action,
+      method: form.method,
+      func: form.func,
+      style: form.style,
+      rtn: form.rtn
+    });
+
+    if(func) func(this.list);
+    
+    if(rtn){
+      if(!rtn.length > 0) return this.main;
+      this.obj={};
+      rtn.forEach(e => {
+        this.obj[e] = this[e];
+      })
+      return this.obj;
     }
   }
 };
 
 class Form{
-  constructor({path, cName, id, name, action, method, style, rtn}){
+  constructor({path, cName, id, name, action, method, style, func, rtn}){
     this.main=document.createElement('form');
     if(cName) this.main.className=cName;
     if(style) this.main.style=style;
@@ -269,6 +434,8 @@ class Form{
         this.obj[e] = this[e];
       })
       return this.obj;
-    }
+    };
+
+    if(func) func(this.main);
   }
 };
