96b2d84e7e986a01ed56912ae61c30f6b9f74f3f
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-28 20:52:45 +0300

    Update indexedDB.js

--- libs/settings/indexedDB.js
+++ libs/settings/indexedDB.js
@@ -451,15 +451,15 @@ class Obs{
   };
 };
 
-function styleChecker(name){
-  for(let i = 0, arr = document.querySelectorAll(`style`); i < arr.length; i++){
-    if(!arr[i].getAttribute('stylename')) continue;
-    if(arr[i].getAttribute('stylename') === name) return true;
-  }
-};
-
 class Css{
-  constructor(name, css){
+  styleChecker(name){
+    for(let i = 0, arr = document.querySelectorAll(`style`); i < arr.length; i++){
+      if(!arr[i].getAttribute('stylename')) continue;
+      if(arr[i].getAttribute('stylename') === name) return true;
+    }
+  }
+  constructor(name, css, check){
+    if(check && this.styleChecker(name)) return;
     this.main=document.createElement('style');
     this.main.textContent=css;
     if(name) this.main.setAttribute('stylename', name);
