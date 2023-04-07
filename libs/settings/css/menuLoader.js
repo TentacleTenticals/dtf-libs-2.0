b2c07008d12461047cf242e691f4f76fdada5a94
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-04-07 16:26:11 +0300

    Update menuLoader.js

--- libs/settings/css/menuLoader.js
+++ libs/settings/css/menuLoader.js
@@ -83,7 +83,7 @@ let menuLoaderCSS = `
   left: 0px;
   z-index: 1000;
   padding: 3px;
-  max-width: 80%;
+  max-width: 60%;
   max-height: 100%;
   box-shadow: 0px 0px 2px 1px rgb(0 0 0);
   overflow-y: auto;
