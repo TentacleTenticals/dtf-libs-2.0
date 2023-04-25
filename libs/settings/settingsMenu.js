9eb7053b327e5debf0b1dbe361bf8b9b2d3ac453
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-04-25 14:12:53 +0300

    Update settingsMenu.js

--- libs/settings/settingsMenu.js
+++ libs/settings/settingsMenu.js
@@ -46,6 +46,26 @@ class SettingsMenu {
           accepted: '.txt',
           text: 'Загрузить настройки из бэкапа',
           onchange: (e) => {
+            function readSettingsBackup(submit, e){
+              let fr = new FileReader();
+              let path = e.target;
+              fr.onloadend = (e) => {
+                // console.log(JSON.parse(e.target.result));
+                mainCfg = JSON.parse(e.target.result);
+                console.log(`Настройки успешно восстановлены.`, mainCfg);
+                new Alert({
+                  type: 'Settings import',
+                  text: 'Настройки успешно импортированы, но не сохранены. Переоткройте окно настроек и удостовертесь в том, что результат вас устраивает, после чего нажмите кнопку сохранения настроек.',
+                  timer: 10000
+                })
+                path.parentNode.children[1].textContent = 'Настройки успешно загружены.';
+                submit.disabled = true;
+              };
+              fr.onerror = (e) => {
+                console.log(e);
+              };
+              fr.readAsText(e.target.files[0]);
+            };
             readSettingsBackup(this.submit, e);
           }
         }
@@ -57,6 +77,18 @@ class SettingsMenu {
       text: 'Бэкап настроек в файл',
       title: 'Сохранение настроек в .txt файл',
       onclick: () => {
+        function backupSettingsToFile(data, filename, type) {
+          let file = new Blob([data], {type: type});
+          if(window.navigator.msSaveOrOpenBlob) window.navigator.msSaveOrOpenBlob(file, filename);
+          else{
+            var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            a.click();
+            window.URL.revokeObjectURL(url);
+          }
+        };
         backupSettingsToFile(JSON.stringify(mainCfg, null, 2), `${mainCfg.srciptInfo.scriptName} ${new Date()} (бэкап настроек).txt`, 'text/plain');
       }
     });
