141210768f3fc8782e84f8bb1396f16eed8133ba
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-07-30 12:58:00 +0300

    Update classes.js

--- libs/splitCls/classes.js
+++ libs/splitCls/classes.js
@@ -139,4 +139,20 @@ class El{
     if(name) this.main.setAttribute('stylename', name);
     document.body.appendChild(this.main);
   };
+  onPageLoad(run){
+    {
+    const log = console.log.bind(console);
+    console.log = (...args) => {
+      if(Array.isArray(args)){
+        if(args[0]){
+          if(typeof args[0] === 'string'){
+            if(args[0].match(/\[ Air \] Ready.*/)){
+              run();
+            }
+          }
+        }
+      }
+      log(...args);
+    }}
+  };
 };
