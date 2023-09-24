0f00a74dea102f84d88dbf1a3f12c7351c038b78
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-09-24 20:17:11 +0300

    Update checkPageType.js

--- func/checkPageType.js
+++ func/checkPageType.js
@@ -6,36 +6,36 @@ function getPageType(url){
   url.replace(/https:\/\/dtf\.ru\/([^]+)/, (d, text) => {
     let arr = text.split('/');
     
-    switch(true){
-      case arr[0] && arr[0].match(/^popular$/):{
+    switch(true && arr[0]){
+      case arr[0] && arr[0].match(/^popular$/)?.input:{
         if(!arr[1]) url = {type: 'popular'};
       }break;
 
-      case arr[0] && arr[0].match(/^new$/):{
+      case arr[0] && arr[0].match(/^new$/)?.input:{
         if(!arr[1]) url = {type: 'new'};
       }break;
 
-      case arr[0] && arr[0].match(/^my$/):{
+      case arr[0] && arr[0].match(/^my$/)?.input:{
         if(arr[1] && arr[1].match(/^new$/)) url = {type: 'my new'};
       }break;
 
-      case arr[0] && arr[0].match(/^bookmarks$/):{
+      case arr[0] && arr[0].match(/^bookmarks$/)?.input:{
         if(!arr[1]) url = {type: 'bookmarks'};
       }break;
 
-      case arr[0] && arr[0].match(/^u$/):{
+      case arr[0] && arr[0].match(/^u$/)?.input:{
         if(arr[1] && !arr[2]) url = {type: 'user page', name: arr[1]};
       else
         if(arr[1] && arr[2]) url = {type: 'topic'};
       }break;
         
-      case arr[0] && arr[0].match(/^s$/):{
+      case arr[0] && arr[0].match(/^s$/)?.input:{
         if(arr[1] && !arr[2]) url = {type: 'subsite', name: arr[1]};
       else
         if(arr[1] && arr[2]) url = {type: 'topic'};
       }break;
         
-      case arr[0] && !arr[0].match(/^(u|s)$/):{
+      case arr[0] && !arr[0].match(/^(u|s)$/)?.input:{
         if(arr[0] && !arr[1]) url = {type: 'subsite', name: arr[0]};
       else
         if(arr[0] && arr[1]) url = {type: 'topic'};
