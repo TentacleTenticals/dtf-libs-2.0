63d73e7d9576c5f3c8d8f479ede05b273c33e452
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-09-25 11:46:41 +0300

    Create opener.js

---
+++ settings/opener.js
@@ -0,0 +1,30 @@
+function SettingsOpener(){
+    new El().Div({
+      path: document.querySelector(`.site-header-container`) ? document.querySelector(`.site-header-container`) : document.body,
+      cName: 'DTF-scriptSettingsOpener',
+      id: 'DTF-scriptSettingsOpener',
+      func: (m) => {
+        new El().Div({
+          path: m,
+          cName: 'header',
+          func: (c) => {
+            new El().Div({
+              path: c,
+              cName: 'label',
+              text: 'Настройки скриптов'
+            });
+            new El().Div({
+              path: c,
+              cName: 'icon',
+              text: '🛠️\uFE0E'
+            });
+          }
+        });
+        new El().Div({
+          path: m,
+          cName: 'list'
+        });
+      }
+    });
+  }
+};
