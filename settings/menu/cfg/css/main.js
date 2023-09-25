d763e2393e08d9721280e556f2f2c323ee951bf0
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-09-25 18:57:02 +0300

    Update main.js

--- settings/menu/cfg/css/main.js
+++ settings/menu/cfg/css/main.js
@@ -74,6 +74,7 @@ const settingsMenuCss = () => `
 
 .dtf-scriptWindow.settings .iList>ul {
   background-color: rgb(0,0,0);
+  box-shadow: 0 0 1px 0px rgb(255,255,255);
 }
 
 .dtf-scriptWindow button {
