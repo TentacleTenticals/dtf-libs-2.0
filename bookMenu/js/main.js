dddf757ba5921b953665cf1576768355b2f7f2b2
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-09-26 17:10:53 +0300

    Update main.js

--- bookMenu/js/main.js
+++ bookMenu/js/main.js
@@ -3,28 +3,28 @@ class BookMenu{
     let t = new Date(d * 1000);
     return `${t.getFullYear()}/${t.getMonth()+1 < 10 ? `0${t.getMonth()+1}` : t.getMonth()+1}/${t.getDate() < 10 ? `0${t.getDate()}` : t.getDate()} ${t.getHours() < 10 ? `0${t.getHours()}` : t.getHours()}:${t.getMinutes() < 10 ? `0${t.getMinutes()}` : t.getMinutes()}:${t.getSeconds() < 10 ? `0${t.getSeconds()}` : t.getSeconds()}`
   };
-  prevItem(path, target, type, mainCfg){
+  prevItem(path, target, type){
     if((+path.children[0].children[0].children[0].children[0].textContent - 1) < 1) return;
     path.children[1].replaceChildren();
     this.rewriteText({target:path.children[0].children[0].children[0].children[0], mode:'--'});
     for(let i = 0, arr = mainCfg.menu[type].sz, length = arr; i < length; i++){
       new MenuItem()[type]({
         path:path.children[1],
-        num:this.getList(path.children[0].children[0].children[0].children[0].textContent-1, i, type, mainCfg),
+        num:this.getList(path.children[0].children[0].children[0].children[0].textContent-1, i, type),
         get item(){
           return target[this.num]
         }
       });
     }
   }
-  nextItem(path, target, type, mainCfg){
+  nextItem(path, target, type){
     if((+path.children[0].children[0].children[0].children[0].textContent + 1) > +path.children[0].children[0].children[0].children[1].textContent) return;
     path.children[1].replaceChildren();
     this.rewriteText({target:path.children[0].children[0].children[0].children[0], mode:'++'});
     for(let i = 0, arr = mainCfg.menu[type].sz, length = arr; i < length; i++){
       new MenuItem()[type]({
         path:path.children[1],
-        num:this.getList(path.children[0].children[0].children[0].children[0].textContent-1, i, type, mainCfg),
+        num:this.getList(path.children[0].children[0].children[0].children[0].textContent-1, i, type),
         get item(){
           return target[this.num];
         },
@@ -39,20 +39,20 @@ class BookMenu{
       this.rewriteText({target:e.children[0].children[0].children[1].children[0], text:'0'});
     }
   };
-  getList(num, i, type, mainCfg){
+  getList(num, i, type){
     return (mainCfg.menu[type].sz * num) + i;
   }
-  itemList({path, target, type, mainCfg}){
+  itemList({path, target, type}){
     console.log('TYPE', type)
     console.log('TARGET', target)
     if(!target && !target.length > 0) return;
     path.parentNode.setAttribute('type', type);
 
     path.children[0].children[1].children[0].onclick = (e) => {
-      this.prevItem(e.target.parentNode.parentNode.parentNode, target, type, mainCfg);
+      this.prevItem(e.target.parentNode.parentNode.parentNode, target, type);
     }
     path.children[0].children[1].children[1].onclick = (e) => {
-      this.nextItem(e.target.parentNode.parentNode.parentNode, target, type, mainCfg);
+      this.nextItem(e.target.parentNode.parentNode.parentNode, target, type);
     }
 
     if(target.length > 0){
@@ -62,7 +62,7 @@ class BookMenu{
       for(let i = 0, arr = mainCfg.menu[type].sz, length = arr; i < length; i++){
         new MenuItem()[type]({
           path:path.children[1],
-          num:this.getList(path.children[0].children[0].children[0].children[0].textContent-1, i, type, mainCfg),
+          num:this.getList(path.children[0].children[0].children[0].children[0].textContent-1, i, type),
           get item(){
             return target[this.num]
           }
@@ -70,7 +70,7 @@ class BookMenu{
       }
     }
   }
-  build({path, cName, target}){
+  build({path, iName, target}){
     this.main=new El().Div({
       path: path,
       cName: 'bookMenu',
@@ -122,7 +122,7 @@ class BookMenu{
               },
               onclick: () => {
                 const arr = JSON.parse(this.main.children[1].getAttribute('picked'));
-                if(!arr && !arr.length > 0) return;
+                if(!arr||arr && !arr.length > 0) return;
                 console.log('ARR', arr);
                 this.main.children[1].setAttribute('picked', '[]');
                 this.rewriteText({target:this.main.children[0].children[0].children[2].children[0], text:'0'});
@@ -170,7 +170,7 @@ class BookMenu{
               text: '🔜\uFE0E',
               onclick: (e) => {
                 console.log('TYPE', target)
-                this.nextItem(e.target.parentNode.parentNode.nextElementSibling, target, e.target.parentNode.parentNode.parentNode.parentNode.getAttribute('type'), mainCfg);
+                this.nextItem(e.target.parentNode.parentNode.nextElementSibling, target, e.target.parentNode.parentNode.parentNode.parentNode.getAttribute('type'));
               }
             });
           }
@@ -179,7 +179,7 @@ class BookMenu{
     });
     new El().Div({
       path: this.main,
-      cName: 'itemsList scrollMid',
+      cName: `itemsList${iName ? ' '+iName : ''} scrollMid`,
       attr: ['picked', '[]']
     });
   }
