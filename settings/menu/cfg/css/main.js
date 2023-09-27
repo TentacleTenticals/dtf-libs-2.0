99dda6c1aabace138f49dcbc72c77d12e455e9a4
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-09-27 12:46:17 +0300

    Update main.js

--- settings/menu/cfg/css/main.js
+++ settings/menu/cfg/css/main.js
@@ -1,5 +1,8 @@
-const settingsMenuCss = () => `
+const cfgMenuCss = () => `
 .dtf-scriptWindow {
+  display: flex;
+  flex-direction: column;
+  gap: 5px 0;
   position: fixed;
   background-color: rgb(0 0 0);
   color: rgb(255 255 255);
@@ -8,19 +11,19 @@ const settingsMenuCss = () => `
   z-index: 1000;
   padding: 3px;
   max-width: 60%;
-  height: 100%;
+  max-height: 95%;
   box-shadow: 0px 0px 2px 1px rgb(0 0 0);
   overflow-y: auto;
   scrollbar-width: thin;
   scrollbar-color: rgb(18, 101, 134) rgb(0, 0, 0);
 }
-.dtf-scriptWindow .header {
-  display: block;
+
+.dtf-scriptWindow>.header {
   position: relative;
   background-color: rgb(40 40 40);
   color: rgb(231 193 255);
   text-align: center;
-  font-size: 17px;
+  font-size: 12px;
   font-weight: 500;
   font-family: 'Chakra Petch', sans-serif;
   letter-spacing: 0.5px;
@@ -29,62 +32,70 @@ const settingsMenuCss = () => `
   cursor: pointer;
 }
 
-.dtf-scriptWindow fieldset {
-  display: flex;
-  background-color: rgb(35 35 35);
-  flex-direction: column;
-  flex-wrap: wrap;
-  padding: 3px;
-  margin: 5px 0px 5px 0px;
-  border: 1px solid rgb(80 80 80);
-  gap: 5px 10px;
-}
-.dtf-scriptWindow fieldset.hide .list {
-  display: none;
-}
-.dtf-scriptWindow fieldset .list {
-  display: flex;
-  flex-direction: column;
-  padding: 5px;
-  background-color: rgb(18 18 18);
-  border-radius: 2px;
-  gap: 5px 15px;
-  box-shadow: 0 0 2px 1px rgb(72 72 72);
-}
-.dtf-scriptWindow fieldset.grid .list {
-  display: grid;
-  align-items: center;
-  grid-template-columns: repeat(2, max-content);
-}
-
-.dtf-scriptWindow>.header {
-  text-align: center;
-  font-weight: 500;
-  padding: 5px 0px 0px;
-}
-
 .dtf-scriptWindow legend {
   font-weight: 500;
   font-size: 15px;
-}
-
-.dtf-scriptWindow legend {
   cursor: pointer;
 }
-
-.dtf-scriptWindow input {
-  padding: 0 2px 0 2px;
-  border: unset;
-  border-radius: 2px;
+.dtf-scriptWindow form .info {
+  font-size: 12px;
+}
+.dtf-scriptWindow form .fList {
+  display: flex;
+  flex-direction: column;
+  gap: 5px 5px;
 }
 
-.dtf-scriptWindow.settings .iList>ul {
+.dtf-scriptWindow :is(input, select) {
+  padding: 0 2px 0 2px;
+  height: fit-content;
+  border: unset;
+  border-radius: 2px;
+  outline: none;
+}
+.dtf-scriptWindow select {
   background-color: rgb(0,0,0);
-  box-shadow: 0 0 1px 0px rgb(255,255,255);
+  color: rgb(255,255,255);
+  box-shadow: 0 0 2px 1px rgb(87 87 87);
+}
+
+.dtf-scriptWindow :is(input, select) {
+  background-color: rgb(41 41 41);
+  color: rgb(255,255,255);
+  box-shadow: inset 0 0 3px 0px rgb(137 137 137);
+}
+
+.dtf-scriptWindow label :is(input, select) {
+  margin: auto 0 auto 0;
 }
 
 .dtf-scriptWindow button {
+  background-color: rgb(0,0,0);
   color: rgb(255,255,255);
-  box-shadow: 0 0 2px 1px rgb(255,255,255);
+  height: fit-content;
+  border: none;
+  border-radius: 2px;
+  box-shadow: 0 0 1px 1px rgb(80,80,80);
+  cursor: pointer;
+}
+
+.dtf-scriptWindow .iList {
+  width: 100%;
+  cursor: cell;
+}
+.dtf-scriptWindow .iList ul {
+  background-color: rgb(30 30 30);
+  box-shadow: 0 0 2px 1px rgb(117 117 117);
+}
+.dtf-scriptWindow .iList ul .value {
+  cursor: text;
+}
+.dtf-scriptWindow .iList ul button {
+  cursor: pointer;
+}
+
+.dtf-scriptWindow .actions {
+  display: flex;
+  gap: 5px 10px;
 }
 `;
