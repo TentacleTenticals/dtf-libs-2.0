c5c94ceb0636e8e66b8c981543179f68aab80f69
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-06 16:14:05 +0300

    Update main.js

--- interface/ctxMenu/css/main.js
+++ interface/ctxMenu/css/main.js
@@ -1,7 +1,26 @@
-const ctxMenuCss = () => `
+const ctxMenuCss = () => {
+return `
 @import url('https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed:wght@400;500;600&display=swap');
 
-.contextMenu {
+.ctx {
+  display: flex;
+  flex-direction: column;
+  gap: 5px 5px;
+  padding: 3px;
+  min-width: 100px;
+  max-width: max-content;
+  box-shadow: 0 0 2px 1px rgb(0,0,0);
+}
+
+.ctx>.header {
+  padding: 1px 3px 1px 3px;
+  text-align: center;
+  border-radius: 2px;
+  background-color: rgb(0,0,0);
+  color: rgb(255,255,255);
+}
+
+.ctx {
   display: flex;
   flex-direction: column;
   gap: 5px 0;
@@ -11,7 +30,7 @@ const ctxMenuCss = () => `
   box-shadow: 0 0 1px 1px rgb(0,0,0);
   z-index: 1000;
 }
-.contextMenu>.header {
+.ctx>.header {
   padding: 0 3px 0 3px;
   font-size: 13px;
   text-align: center;
@@ -22,14 +41,14 @@ const ctxMenuCss = () => `
   color: rgb(255,255,255);
 }
 
-.contextMenu .list {
+.ctx .list {
   display: flex;
   flex-direction: column;
   gap: 3px 3px;
   padding: 0 3px 3px 3px;
 }
 
-.contextMenu .list button {
+.ctx .list button {
   background-color: rgb(0,0,0);
   color: rgb(255,255,255);
   border: unset;
@@ -39,7 +58,7 @@ const ctxMenuCss = () => `
   cursor: pointer;
 }
 
-.contextMenu .separator {
+.ctx .separator {
   font-size: 12px;
   background-color: rgb(205 178 219);
   color: rgb(0,0,0);
@@ -47,7 +66,7 @@ const ctxMenuCss = () => `
   border-radius: 2px;
 }
 
-.contextMenu .list .sub {
+.ctx .list .sub {
   display: flex;
   position: relative;
   background-color: rgb(0,0,0);
@@ -56,7 +75,7 @@ const ctxMenuCss = () => `
   justify-content: center;
   box-shadow: inset 0 0 3px 0 rgb(255,255,255);
 }
-.contextMenu .list .sub .header {
+.ctx .list .sub .header {
   display: flex;
   align-items: center;
   gap: 0 3px;
@@ -65,7 +84,7 @@ const ctxMenuCss = () => `
   font-weight: 600;
   font-family: 'Fira Sans Extra Condensed', sans-serif;
 }
-.contextMenu .list .sub .header::after {
+.ctx .list .sub .header::after {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -77,20 +96,20 @@ const ctxMenuCss = () => `
   margin: auto;
   border-radius: 50%;
 }
-.contextMenu .list .sub .subList {
+.ctx .list .sub .subList {
   display: none;
   position: absolute;
   left: 100%;
 }
-.contextMenu .list .sub:hover .subList {
+.ctx .list .sub:hover .subList {
   display: flex;
 }
-.contextMenu .list .sub:hover .list {
+.ctx .list .sub:hover .list {
   display: flex;
   flex-direction: column;
   padding: 3px 3px 3px 3px;
   margin: -3px 0 0 3px;
-  background-color: rgb(200,0,0);
+  background-color: rgb(0,0,0);
 }
-
 `;
+};
