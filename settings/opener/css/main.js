f01df2ab964621b0cc7a5521e77f86c2285ad10a
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-09 11:57:51 +0300

    Update main.js

--- settings/opener/css/main.js
+++ settings/opener/css/main.js
@@ -4,21 +4,15 @@ const openerCss = () => `
   flex-direction: column;
   gap: 5px 0;
   position: absolute;
-  top: 33%;
   right: 20%;
   width: fit-content;
   padding: 2px;
   border-radius: 1px;
-  background-color: rgb(255,255,255);
   box-shadow: 0 0 2px 1px rgb(0,0,0);
-  z-index: 100;
 }
 .DTF-scriptSettingsOpener>.header {
   display: flex;
   justify-content: center;
-  background-color: rgb(0,0,0);
-  color: rgb(255,255,255);
-  border-radius: 3px;
 }
 
 .DTF-scriptSettingsOpener .header>.label {
@@ -26,12 +20,6 @@ const openerCss = () => `
 }
 .DTF-scriptSettingsOpener:hover .header>.label {
   display: block;
-}
-.DTF-scriptSettingsOpener .header>.icon {
-  display: flex;
-  font-size: 15px;
-  line-height: 15px;
-  padding: 3px 0 0 0;
 }
 
 .DTF-scriptSettingsOpener .header+.list {
@@ -46,12 +34,16 @@ const openerCss = () => `
 .DTF-scriptSettingsOpener .header+.list>.container {
   display: flex;
   flex-direction: row;
+  align-items: center;
   gap: 0 3px;
+  padding: 0 5px 0 5px;
   background-color: rgb(221 239 249);
   box-shadow: 0 0 2px 0px rgb(0,0,0);
 }
 
-.DTF-scriptSettingsOpener .scriptName {
-  font-size: 13px;
-  font-weight: 600;
+.DTF-scriptSettingsOpener .list>.container button {
+  cursor: pointer;
+}
+.DTF-scriptSettingsOpener .list>.container button:hover {
+  opacity: 0.5;
 }`;
