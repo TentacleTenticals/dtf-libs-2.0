25c9810781e4f2a55d1a13de7ea6461166ad8cec
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-12 21:39:31 +0300

    Update checkPageType.js

--- func/checkPageType.js
+++ func/checkPageType.js
@@ -4,41 +4,44 @@ function getPageType(url){
     return;
   }
   url.replace(/https:\/\/dtf\.ru\/([^]+)/, (d, text) => {
-    let arr = text.split('/');
+    function ch(t){
+      if(t) return true;
+    }
+    const arr = text.split('/');
     
     switch(true && true){
-      case arr[0] && arr[0].match(/^popular$/)?.input:{
-        if(!arr[1]) url = {type: 'popular'};
+      case arr[0] && ch(arr[0].match(/^popular$/)?.input):{
+        if(!arr[1]) return url = {type: 'popular'};
       }break;
 
-      case arr[0] && arr[0].match(/^new$/)?.input:{
-        if(!arr[1]) url = {type: 'new'};
+      case arr[0] && ch(arr[0].match(/^new$/)?.input):{
+        if(!arr[1]) return url = {type: 'new'};
       }break;
 
-      case arr[0] && arr[0].match(/^my$/)?.input:{
-        if(arr[1] && arr[1].match(/^new$/)) url = {type: 'my new'};
+      case arr[0] && ch(arr[0].match(/^my$/)?.input):{
+        if(arr[1] && arr[1].match(/^new$/)) return url = {type: 'my new'};
       }break;
 
-      case arr[0] && arr[0].match(/^bookmarks$/)?.input:{
-        if(!arr[1]) url = {type: 'bookmarks'};
+      case arr[0] && ch(arr[0].match(/^bookmarks$/)?.input):{
+        if(!arr[1]) return url = {type: 'bookmarks'};
       }break;
 
-      case arr[0] && arr[0].match(/^u$/)?.input:{
-        if(arr[1] && !arr[2]) url = {type: 'user page', name: arr[1]};
+      case arr[0] && ch(arr[0].match(/^u$/)?.input):{
+        if(arr[1] && !arr[2]) return url = {type: 'user page', name: arr[1]};
       else
-        if(arr[1] && arr[2]) url = {type: 'topic'};
+        if(arr[1] && arr[2]) return url = {type: 'topic'};
       }break;
         
-      case arr[0] && arr[0].match(/^s$/)?.input:{
-        if(arr[1] && !arr[2]) url = {type: 'subsite', name: arr[1]};
+      case arr[0] && ch(arr[0].match(/^s$/)?.input):{
+        if(arr[1] && !arr[2]) return url = {type: 'subsite', name: arr[1]};
       else
-        if(arr[1] && arr[2]) url = {type: 'topic'};
+        if(arr[1] && arr[2]) return url = {type: 'topic'};
       }break;
         
-      case arr[0] && !arr[0].match(/^(u|s)$/)?.input:{
-        if(arr[0] && !arr[1]) url = {type: 'subsite', name: arr[0]};
+      case arr[0] && ch(!arr[0].match(/^(u|s)$/)?.input):{
+        if(arr[0] && !arr[1]) return url = {type: 'subsite', name: arr[0]};
       else
-        if(arr[0] && arr[1]) url = {type: 'topic'};
+        if(arr[0] && arr[1]) return url = {type: 'topic'};
       }break;
         
     }
