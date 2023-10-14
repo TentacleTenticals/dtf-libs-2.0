a0142ac6a96cfee1c20a906c59bc86fe30d68b53
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-14 11:32:42 +0300

    Create main.js

---
+++ interface/alerter/css/main.js
@@ -0,0 +1,94 @@
+const alerterCss = () => {
+    return `
+  .dtf-buttonsField {
+    display: inline-flex;
+    flex-direction: column;
+    gap: 5px 0px;
+    position: fixed;
+    width: max-content;
+    height: 100%;
+    top: 60px;
+    right: 0;
+    padding: 3px 3px 0 3px;
+    overflow: hidden;
+    /* background: red; */
+    z-index: 1000;
+  }
+
+  .dtf-alert {
+    display: block;
+    position: relative;
+    top: 0;
+    width: 290px;
+    min-height: 60px;
+    height: max-content;
+    margin: 0px 0px 0px 0px;
+    box-shadow: 0px 0px 3px 1px rgb(0 0 0);
+    padding: 3px;
+    animation-duration: 2s;
+    animation-delay: 0s;
+    animation-iteration-count: 1;
+    animation-direction: alternate;
+    animation-name: slideIn;
+    z-index: 10;
+    opacity: 0.9;
+  }
+  .dtf-alert .header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+
+  .dtf-alert.hide {
+    right: -100%;
+    animation-duration: 2s;
+    animation-delay: 0s;
+    animation-iteration-count: 1;
+    animation-direction: alternate;
+    animation-name: slideOut;
+  }
+  .dtf-alert :is(.type, .scriptName) {
+    width: max-content;
+    font-size: 13px;
+    font-weight: 600;
+    padding: 0px 2px 0px 2px;
+    border-radius: 3px;
+  }
+
+  .dtf-alert .text {
+    padding: 3px;
+    font-size: 15px;
+    white-space: pre-wrap;
+  }
+  .dtf-alert .text::before {
+    display: block;
+    content: '';
+    width: 100%;
+    height: 4px;
+    box-shadow: inset 0px 0px 3px 1px rgb(46 46 46);
+  }
+
+  .dtf-alert.info {
+    background-color: rgb(229 140 140);
+  }
+  .dtf-alert.err {
+    background-color: rgb(154 235 154);
+  }
+
+  @keyframes slideIn {
+    from {
+      right: -100%;
+    }
+    to {
+      right: calc(0%);
+    }
+  }
+  @keyframes slideOut {
+    from {
+      right: calc(-100% + 250px);
+    }
+    to {
+      right: -100%;
+    }
+  }`
+  }
