0f5580f4212f0bbe22a863f75fee5c5a293b80a3
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-17 10:37:52 +0300

    Update main.js

--- settings/opener/menu/data/css/main.js
+++ settings/opener/menu/data/css/main.js
@@ -1,7 +1,20 @@
 const dataMenuCss = () => `
 .dtf-scriptWindow.data {
-  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px 0;
+  position: fixed;
+  background-color: rgb(0 0 0);
+  color: rgb(255 255 255);
+  top: 0px;
+  left: 0px;
+  z-index: 1000;
+  padding: 3px;
+  max-width: 90%;
   height: 100%;
-  background-color: rgb(26 5 21);
+  box-shadow: 0px 0px 2px 1px rgb(0 0 0);
+  overflow-y: auto;
+  scrollbar-width: thin;
+  scrollbar-color: rgb(18, 101, 134) rgb(0, 0, 0);
 }
 `;
