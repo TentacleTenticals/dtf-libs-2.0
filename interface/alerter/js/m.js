e032093be88fb2d99f416d7e787c8a6c6bcc7576
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-02-19 10:07:11 +0300

    Create m.js

---
+++ interface/alerter/js/m.js
@@ -0,0 +1,51 @@
+export class Alerter{
+  constructor(o){
+    if(!document.getElementById('alerterField')){
+      El.Div({
+        path: document.body,
+        id: 'alerterField'
+      });
+    }
+    this.build(o);
+  }
+  build(o){
+    El.Div({
+      path: document.getElementById('alerterField'),
+      class: 'alerter' + (!o.alert ? ' info' : ' err'),
+      func: (m) => {
+        
+        El.Div({
+          path: m,
+          class: 'header',
+          func: (h) => {
+            El.Div({
+              path: h,
+              class: 'type',
+              text: o.title
+            });
+            El.Div({
+              path: h,
+              class: 'scriptName',
+              text: o.GM && o.GM.info.script.author||info.scriptInfo.name
+            });
+          }
+        });
+
+        El.Div({
+          path: m,
+          class: 'text',
+          text: o.text
+        });
+        
+        if(o.timer){
+          setTimeout(() => {
+            m.classList.add('hide');
+            setTimeout(() => {
+              m.remove();
+            }, 2000);
+          }, o.timer);
+        }
+      }
+    });
+  }
+}
